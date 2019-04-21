04f98b52c20e2ae27efeef94d8acca99bf206fcb
Kirby <fitchkd25@gmail.com>
2019-04-21 06:40:23 -0500

    stream secret
    stream secret

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose';
-
+import { Canvas } from 'mason-library';
 import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
+import { StreamApp, NotificationDropdown, FlatFeed } from 'react-activity-feed';
+import 'react-activity-feed/dist/index.css';
 
 class HomePage extends Component {
   constructor(props) {
@@ -25,14 +27,19 @@ class HomePage extends Component {
     this.props.firebase.users().off();
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
-      </div>
-    );
-  }
+    render() {
+      return(
+        <StreamApp
+          apiKey="wzty4shbuqbn"
+          appId="50683"
+          token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidXNlci1vbmUifQ.qaediIXzt-cGiPP_istWeMEgEJ9HTy13Tn2VgXmpzks"
+        >
+          <FlatFeed
+            notify
+          />
+        </StreamApp>
+      );
+    }
 }
 
 const condition = authUser => !!authUser;
@@ -41,3 +48,5 @@ export default compose(
   withFirebase,
   withAuthorization(condition),
 )(HomePage);
+
+//  note note note note
