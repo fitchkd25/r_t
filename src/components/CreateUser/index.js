2c30d96479179e4d12eb030444f2ca18632988fa
Kirby <fitchkd25@gmail.com>
2019-04-28 20:25:41 -0500

    paperform working
    paperform

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import { ReactTypeformEmbed } from 'react-typeform-embed';
+import * as typeForm from './typeForm.js';
 
 class CreateUser extends React.Component {
-  constructor(props) {
-    super(props);
-    this.openForm = this.openForm.bind(this);
-  }
-
-  openForm() {
-    this.typeformEmbed.typeform.open();
-  }
 
   render() {
     return (
-      <div>
-        <ReactTypeformEmbed
-          autoOpen={true}
-          url="https://kirbyjrfitch.typeform.com/to/nh3QQU"
-          hideHeaders
-          hideFooter
-          buttonText="Go!"
-          style={{ top: 100 }}
-          ref={tf => {
-            this.typeformEmbed = tf;
-          }}
-        />
-      </div>
+      <body>
+      <div data-paperform-id="roadmap-tools-createuser"></div>
+      <script>typeForm();</script>
+      </body>
     );
   }
 }
---
+++ src/components/CreateUser/typeForm.js
@@ -0,0 +1,1 @@
+(function() {var script = document.createElement('script'); script.src = "https://paperform.co/__embed"; document.body.appendChild(script); })()
