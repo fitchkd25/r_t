dcf49ace17f82f3eae3154713c3d25157498c2e4
Kirby <fitchkd25@gmail.com>
2019-04-28 21:35:53 -0500

    typeform working
    working

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -6,8 +6,7 @@ class CreateUser extends React.Component {
   render() {
     return (
       <body>
-      <div data-paperform-id="roadmap-tools-createuser"></div>
-      <script>typeForm();</script>
+        <div class="typeform-widget" data-url="https://kirbyjrfitch.typeform.com/to/nh3QQU" data-transparency="50" data-hide-headers="true" data-hide-footer="true" style={{width: '100%', height: '500px'}}></div>
       </body>
     );
   }
---
+++ src/components/CreateUser/index_old2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as paperForm from './paperForm.js';
+
+class CreateUser extends React.Component {
+
+  render() {
+    return (
+      <body>
+      <div data-paperform-id="roadmap-tools-createuser"></div>
+      </body>
+    );
+  }
+}
+
+export default CreateUser;
