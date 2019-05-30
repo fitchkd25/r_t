eb34cc8a2a46dc8550c8ee799b594ebab7f778f4
Kirby <fitchkd25@gmail.com>
2019-05-29 22:39:35 -0500

    SPA working on nav bar!
    SPA working on nav bar!

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Canvas } from 'mason-library';
-
 import { AuthUserContext } from '../Session';
-import * as ROUTES from '../../constants/routes';
-import * as ROLES from '../../constants/roles';
 
-const Navigation = () => (
-  <AuthUserContext.Consumer>
-    {authUser =>
-      authUser ? (
-        <NavLoggedIn authUser={authUser} />
-      ) : (
-        <NavLoggedOut />
-      )
-    }
-  </AuthUserContext.Consumer>
-);
 
-const NavLoggedIn = ({ authUser }) => (<Canvas id='5ca92a96f1e3fc000332dc7f' />);
-const NavLoggedOut = () => (<Canvas id='5c90592d6c1eab0003862eae' />);
+class Navigation extends Component {
+  render() {
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            <Canvas
+              id="5ca92a96f1e3fc000332dc7f"
+              eventHandlers={{
+                onNavLinkClick: (e) => {
+                    e.preventDefault();
+                    this.props.history.push(e.target.getAttribute('href'));
+                }
+             }} 
+             />
+          </div>
+        )}
+      </AuthUserContext.Consumer>
+    )
+  }
+}
 
-export default Navigation;
+export default withRouter(Navigation);
